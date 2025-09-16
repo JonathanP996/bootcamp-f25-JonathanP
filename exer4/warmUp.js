a8404a4529404baf3bb0404141f551be777df326
Jonathan Pattassery <jonathanpattassery@icloud.com>
2025-09-16 17:45:35 -0400

    warmup.js

--- exer4/warmUp.js
+++ exer4/warmUp.js
@@ -16,7 +16,21 @@
  * F = anything < 60
  */
 function toLetterGrade(numGrade) {
-  // TODO
+  if (numGrade < 0 || numGrade > 100) {
+    return "INVALID";
+  }
+  
+  if (numGrade >= 90) {
+    return "A";
+  } else if (numGrade >= 80) {
+    return "B";
+  } else if (numGrade >= 70) {
+    return "C";
+  } else if (numGrade >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
 }
 
 /**
@@ -27,7 +41,13 @@ function toLetterGrade(numGrade) {
  * Example: [9, 3, 4, 1, 2, 0] --> [4, 2, 0]
  */
 function getEvenElements(array) {
-  // TODO
+  let evenArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      evenArray.push(array[i]);
+    }
+  }
+  return evenArray;
 }
 
 /**
@@ -40,7 +60,16 @@ function getEvenElements(array) {
  * Example: "I love Bits of Good" --> 4
  */
 function findLongestWord(string) {
-  // TODO
+  let words = string.split(" ");
+  let longestLength = 0;
+  
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestLength) {
+      longestLength = words[i].length;
+    }
+  }
+  
+  return longestLength;
 }
 
 /**
@@ -65,7 +94,17 @@ function findLongestWord(string) {
     } 
  */
 function combineObjects(object1, object2) {
-  // TODO
+  let combined = {};
+  
+  for (let key in object1) {
+    combined[key] = object1[key];
+  }
+  
+  for (let key in object2) {
+    combined[key] = object2[key];
+  }
+  
+  return combined;
 }
 
 /**
@@ -77,7 +116,13 @@ function combineObjects(object1, object2) {
  *  Example: [1, 2, 3] -> [3, 2, 1]
  */
 function reverseArr(array) {
-  // TODO
+  let reversed = [];
+  
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversed.push(array[i]);
+  }
+  
+  return reversed;
 }
 
 // DO NOT EDIT BELOW THIS LINE -- the code is for testing purposes only!
