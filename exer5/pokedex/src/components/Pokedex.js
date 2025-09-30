4cf1b49083ea7026980c0cf71253d8a3b71deaf0
Jonathan Pattassery <jonathanpattassery@icloud.com>
2025-09-30 17:58:37 -0400

    color badges added

--- exer5/pokedex/src/components/Pokedex.js
+++ exer5/pokedex/src/components/Pokedex.js
@@ -34,6 +34,30 @@ const Pokedex = () => {
     setPokemonId(pokemonId + 1);
   };
 
+  const getTypeColor = (typeName) => {
+    const typeColors = {
+      normal: '#A8A878',
+      fire: '#F08030',
+      water: '#6890F0',
+      electric: '#F8D030',
+      grass: '#78C850',
+      ice: '#98D8D8',
+      fighting: '#C03028',
+      poison: '#A040A0',
+      ground: '#E0C068',
+      flying: '#A890F0',
+      psychic: '#F85888',
+      bug: '#A8B820',
+      rock: '#B8A038',
+      ghost: '#705898',
+      dragon: '#7038F8',
+      dark: '#705848',
+      steel: '#B8B8D0',
+      fairy: '#EE99AC'
+    };
+    return typeColors[typeName] || '#A8A878';
+  };
+
   if (loading) {
     return <div className="pokedex-container">Loading...</div>;
   }
@@ -63,9 +87,15 @@ const Pokedex = () => {
           
           <div className="pokemon-types">
             <span className="types-label">Types:</span>
-            <span className="type-badge normal">
-              normal
-            </span>
+            {pokemon.types.map((type, index) => (
+              <span 
+                key={index}
+                className="type-badge"
+                style={{ backgroundColor: getTypeColor(type.type.name) }}
+              >
+                {type.type.name}
+              </span>
+            ))}
           </div>
           
           <div className="navigation-buttons">
